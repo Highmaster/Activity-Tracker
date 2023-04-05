2cfe0fe38dd3fa152e1d76d32e3fbdb496c99e2c
Highmaster <nzechideraann@gmail.com>
2023-04-05 18:07:45 +0100

    First Commit

---
+++ src/main/java/com/trackingappwk8/TrackingAppWk8Application.java
@@ -0,0 +1,13 @@
+package com.trackingappwk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingAppWk8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingAppWk8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/trackingappwk8/TrackingAppWk8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackingappwk8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingAppWk8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
